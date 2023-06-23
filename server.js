dcf9b3ae234793187aa41dd91bdb62c617cb678e
major jerk <shreyasrivastava28@gmail.com>
2023-06-23 12:59:18 +0530

    added dockerfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config({path:path.join(__dirname,"config.env")})
 
 const PORT = process.env.PORT || 8000
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.send("hello world!")
 })
 
 app.listen(PORT,()=>{
