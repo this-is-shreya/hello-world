d08080052d7b9ec5e1e17b8316b8c9f51a524759
major jerk <shreyasrivastava28@gmail.com>
2023-06-23 13:10:29 +0530

    modiefied server.js

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:16.16.0
 WORKDIR /app
-COPY . .
 RUN npm install
+COPY . .
 EXPOSE 3000
 CMD ["node","server.js"]
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")
 const path = require("path")
 dotenv.config({path:path.join(__dirname,"config.env")})
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 app.get("/",(req,res)=>{
     res.send("hello world!")
 })
