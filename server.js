337d62f7056b2729e62e97371c45028199fe590c
major jerk <shreyasrivastava28@gmail.com>
2023-06-23 11:37:06 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("express")()
+const dotenv = require("dotenv")
+const path = require("path")
+dotenv.config({path:path.join(__dirname,"config.env")})
+
+const PORT = process.env.PORT || 8000
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(PORT,()=>{
+    console.log("listening at "+ PORT)
+})
